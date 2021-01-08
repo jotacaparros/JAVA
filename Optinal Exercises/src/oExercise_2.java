ba2c837f2de96428e2f32793a6fee81f49bc9f8f
Jota Caparrós <jotacaparros@gmail.com>
2021-01-08 02:26:24 +0100

    OExer

---
+++ Optinal Exercises/src/oExercise_2.java
@@ -0,0 +1,7 @@
+
+/* Create a program that asks numbers to the user until he types a 0. Then, it
+will print the total amount of numbers that the user has typed, apart from the
+last one (the 0) that must be excluded. */
+
+public class oExercise_2 {
+}
