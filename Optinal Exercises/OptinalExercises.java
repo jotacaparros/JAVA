3722809005b3ecdac60b0300965a3bae26ec6293
jota <jotacaparros@gmail.com>
2021-02-07 14:21:41 +0100

    Ejercicio para el viernes

---
+++ Optinal Exercises/OptinalExercises.java
@@ -0,0 +1,9 @@
+/* Create a class called Window. It will have 3 attributes: name (string),
+width (int) and height (int) and a constructor with 3 parameters to
+initialize its attributes. It must have as well a method called
+toString() that returns the information of the window. Create a Main
+class where it will have an array of 10 windows. Each width and
+height will be assigned using a random number. The width will be a
+number between 90 and 120 cm and the height will be a number
+between 40 and 100 cm  */
+
